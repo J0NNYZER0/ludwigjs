6f1b1566115665ee453c83f6f0166cd0036f06f8
J0NNYZER0 <jon.ortiz@me.com>
2018-11-11 09:38:10 -0800

    change config var name

--- scripts/set-npmrc.js
+++ scripts/set-npmrc.js
@@ -1,9 +1,13 @@
-if(process.env.NODE_ENV !== 'production') {
-  return;
+'use strict'
+
+if (process.env.NODE_ENV !== 'production') {
+
+  return
+
 }
 
 const Fs = require('fs')
 
-Fs.writeFileSync('.npmrc', '//registry.npmjs.org/:_authToken=${NPM_TOKEN_ROCKSTAR}')
+Fs.writeFileSync('.npmrc', '//registry.npmjs.org/:_authToken=${NPM_TOKEN}')
 
 Fs.chmodSync('.npmrc', 0600)
