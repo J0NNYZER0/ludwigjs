3270e09b7041cbd5744e6429bef9c4ec3fb904f4
J0NNYZER0 <jon.ortiz@me.com>
2018-11-27 11:38:58 -0800

    change database directory structure

--- database/local/options.js
+++
@@ -1,8 +0,0 @@
-const Path = require('path')
-
-module.exports =  {
-  config: Path.join(__dirname, 'config.json'),
-  'migrations-path': Path.join(__dirname, 'migrations'),
-  'seeders-path': Path.join(__dirname, 'seeders'),
-  'models-path': Path.join(__dirname, 'model'),
-}
