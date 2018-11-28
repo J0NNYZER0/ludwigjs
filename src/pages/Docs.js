b73fd41761bb20e927d9cae108615fa8780d844d
J0NNYZER0 <jon.ortiz@me.com>
2018-11-28 11:58:51 -0800

    begin style of docs page

--- src/pages/Docs.js
+++ src/pages/Docs.js
@@ -7,33 +7,120 @@ class Docs extends Component {
 
     super(props)
 
+    this.state = {
+      show: false
+    }
+
   }
+
+
 
   render() {
 
     return (
-      <div>
-        <section className='t-mrg bolt-btm bolt-top'>
-          <Elements.PageTitle titleText={`Docs`} />
-        </section>
-        <section className='t-mrg bolt-btm'>
-          <Slideshow>
-            <Hero key={1} {...this.props}
-              classNames='cogs'
-              text={`I Was a Mindless Cog in the Machine`}>
-              <div className='banner cog' />
-            </Hero>
-            <Hero key={2} {...this.props}
-              classNames='bolts'
-              text={`Now I Am a Magical Full-stack Beast`}>
-              <div className='banner beastie' />
-            </Hero>
-          </Slideshow>
-        </section>
-        <section>
-          <h3>Say Hello</h3>
-          <Forms.Contact {...this.props} formTitle={``} subject='General Request' contactType={0} />
-        </section>
+      <div className='docs-page'>
+        <div className='panels col-2'>
+          <div className='panel'>
+            <div className='sidebar'>
+              <nav>
+                <ul>
+                  <li>
+                    <span>Ludwig v1.0.0</span>
+                  </li>
+                </ul>
+                <ul>
+                  <li className='show'>
+                    <span>Getting Started</span>
+                    <ul>
+                      <li>
+                        About
+                      </li>
+                      <li>
+                        Requirements
+                      </li>
+                      <li>
+                        Installation
+                      </li>
+                      <li>
+                        Usage
+                      </li>
+                      <li>
+                        Options
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <span>Workflow</span>
+                  </li>
+                  <li>
+                    <span>Pages</span>
+                  </li>
+                  <li>
+                    <span>Components</span>
+                  </li>
+                  <li>
+                    <span>Layouts</span>
+                  </li>
+                  <li>
+                    <span>Resources</span>
+                  </li>
+                  <li>
+                    <span>Themes</span>
+                  </li>
+                  <li>
+                    <span>Plugins</span>
+                  </li>
+                  <li>
+                    <span>Webpack</span>
+                  </li>
+                  <li>
+                    <span>Deployment</span>
+                  </li>
+                  <li>
+                    <span>License</span>
+                  </li>
+                  <li>
+                    <span>Changes</span>
+                  </li>
+                  <li>
+                    <span>Support</span>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+          <div className='panel'>
+            <section className='t-mrg bolt-btm bolt-top'>
+              <Elements.PageTitle titleText={`Docs`} />
+            </section>
+            <section>
+              <h1>{`Getting Started`}</h1>
+            </section>
+            <section>
+              <h2>{`About`}</h2>
+              <p>
+                {`Some helpful text.`}
+              </p>
+            </section>
+            <section>
+              <h2>{`Requirements`}</h2>
+              <p>
+                {`Some helpful text.`}
+              </p>
+            </section>
+            <section className='t-mrg bolt-btm'>
+              <Hero key={1} {...this.props}
+                classNames='cogs'
+                text={`So Easy, a Frikkin' Monkey Can Do It`}>
+                <div className='banner code-monkey' />
+              </Hero>
+            </section>
+            <section>
+              <h3>Say Hello</h3>
+              <Forms.Contact {...this.props} formTitle={``} subject='General Request' contactType={0} />
+            </section>
+          </div>
+        </div>
       </div>
     )
   }
